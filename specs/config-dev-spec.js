85b0efc7bc5590bda32865ead4440c6a26b800e9
WIL PANNELL <wil.pannell@gmail.com>
2015-09-20 21:44:00 -0700

    replace class with revealing module

--- specs/config-dev-spec.js
+++ specs/config-dev-spec.js
@@ -1,29 +1,15 @@
 var expect = require('chai').expect;
 var config;
 
-describe('environments:', function () {
-  describe('dev has configuration', function () {
-    process.env.NODE_ENV = 'dev';
-    config = require('../lib/config');
+describe('dev environment has configuration', function () {
+  //process.env.NODE_ENV = 'test';
+  config = require('../lib/config');
 
-    it('application name', function () {
-      expect(config.get('application:name')).to.equal('vision');
-    });
-
-    it('port', function () {
-      expect(config.get('express:port')).to.equal(3000);
-    });
+  it('application name', function () {
+    expect(config.get('application:name')).to.equal('vision');
   });
-  describe('test has configuration', function () {
-    process.env.NODE_ENV = 'test';
-    config = require('../lib/config');
 
-    it('application name', function () {
-      expect(config.get('application:name')).to.equal('vision');
-    });
-
-    it('port', function () {
-      expect(config.get('express:port')).to.equal(4000);
-    });
+  it('port', function () {
+    expect(config.get('express:port')).to.equal(4000);
   });
 });
